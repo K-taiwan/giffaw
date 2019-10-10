5dbae66d2fa1e914a47d5d9cf06011cdbdb6758b
KennyK-TW <b2khakashu@yahoo.com>
2019-10-09 17:37:17 -0700

    25 showing

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,30 @@
-$(document).on("ready", function(){
+// $(document).on("ready", function(){
 
+// });
+
+
+
+
+const onSuccess = (gif)=>{
+    console.log(gif);
+    gif.data.forEach((data)=>{
+        const template = `<img src="${data.images.fixed_height.url}"></img>`;
+        $('.gif-gallery').append(template);
+    });
+};
+
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+};
+
+$.ajax({
+    method: "GET",
+    url: "http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC",
+    data: $("form").serialize(),
+    success: onSuccess,
+    error: onError,
 });
