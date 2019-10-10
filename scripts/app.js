a14f1f0e58f4010550d8ad1dd425641d4b86d621
KennyK-TW <b2khakashu@yahoo.com>
2019-10-09 17:59:51 -0700

    trying to add search

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,7 @@
 
 const onSuccess = (gif)=>{
     console.log(gif);
+    $('.gif-gallery').empty();
     gif.data.forEach((data)=>{
         const template = `<img src="${data.images.fixed_height.url}"></img>`;
         $('.gif-gallery').append(template);
@@ -21,10 +22,19 @@ function onError(xhr, status, errorThrown) {
     console.dir(xhr);
 };
 
+// $.ajax({
+//     method: "GET",
+//     url: "http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC",
+//     data: $("form").serialize(),
+//     success: onSuccess,
+//     error: onError,
+// });
+
 $.ajax({
     method: "GET",
-    url: "http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC",
+    url: "http://api.giphy.com/v1/gifs/search",
     data: $("form").serialize(),
     success: onSuccess,
     error: onError,
 });
+
