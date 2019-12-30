354abb9f446567c514b1dd61bb909a7822071d3e
KennyK-TW <b2khakashu@yahoo.com>
2019-12-30 13:57:54 -0800

    1st commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,19 @@
 // $(document).on("ready", function(){
 
 // });
+$(document).on("ready", function() {
+    $(".moreBox").slice(0, 3).show();
+      if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+      }   
+      $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 6).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+          $("#loadMore").fadeOut('slow');
+        }
+    });
+});
 
 
 
@@ -22,6 +35,7 @@ function onError(xhr, status, errorThrown) {
     console.dir(xhr);
 };
 
+// $('.text-center').append
 // $.ajax({
 //     method: "GET",
 //     url: "http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC",
